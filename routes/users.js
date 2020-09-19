f0df9b363948d92bcfcaa180261917b4a718d350
Keith Ong <keith.ong5000@gmail.com>
2020-09-19 20:53:37 +0000

    signup route returns an error if a name, username, password or organization_id is not provided

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,13 @@
 const express = require('express');
 const router = express.Router();
 
+const checkAllUserParamsExist = (user) => {
+  if(user.username && user.name && user.password && user.organization_id){
+    return true;
+  }
+  return false;
+}
+
 module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM users;`)
@@ -23,12 +30,18 @@ module.exports = (db) => {
   });
 
   router.post("/", (req, res) => {
+    const newUser = req.body;
+    if(!checkAllUserParamsExist(newUser)){
+      return res
+      .status(500)
+      .json({ error: "Request is missing a field (name, username, password, organization_id)"});
+    }
     const queryString =
       `SELECT username
        FROM users
        WHERE username = $1
       `
-    const queryParams = [req.body.username]
+    const queryParams = [newUser.username]
     db.query(queryString, queryParams)
       .then(data => {
         const matchingUser = data.rows;
@@ -37,6 +50,13 @@ module.exports = (db) => {
             .status(500)
             .json({ error: "Username already exists" });
         }
+        const signupQueryString = `
+        INSERT INTO users ( username, name, password, organization_id)
+        VALUES ($1, $2, $3, $4);
+        `
+        const signupQueryParams = [newUser.username, newUser.name, newUser.password, newUser.organization_id];
+        db.query(queryString, queryParams)
+
         return res.json({message: "SUCCESFULLY CREATED USER"});
       })
       .catch(err => {
