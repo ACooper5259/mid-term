4fad83f1690b74c2dc956884117158adfb11fa1e
JaeyoungKim <jzero.youngs89@gmail.com>
2020-09-21 00:02:15 +0000

    Password Generator function added by Sep 20

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -17,12 +17,23 @@ $(document).ready(function() {
 
   workCheck();
 
-  console.log('before sumbit');
   $("#generate-password").on('submit',function(e) {
-    console.log('inside the function')
-    console.log(e);
     e.preventDefault();
+    let numCheck = $(".number").is(":checked") ? "true" : "false";
+    let lowercaseCheck = $(".lowercase").is(":checked") ? "true" : "false";
+    let uppercaseCheck = $(".uppercase").is(":checked") ? "true" : "false";
+    let specialCheck = $(".special-characters").is(":checked") ? "true" : "false";
+    const totalLength = $(this).find('.length').val();
+    console.log(totalLength);
+    console.log(numCheck, lowercaseCheck, uppercaseCheck);
+    const result = generatedPassword(totalLength, numCheck, lowercaseCheck, uppercaseCheck, specialCheck);
+    console.log(result);
+    $(".password").text(result);
+    $(".length").val('');
+    $(".number").prop("checked", false);
+    $(".lowercase").prop("checked", false);
+    $(".uppercase").prop("checked", false);
+    $(".special-characters").prop("checked", false);
   } )
 
-  console.log('after submit')
 });
--- public/scripts/helper.js
+++ public/scripts/helper.js
@@ -1,38 +1,40 @@
-const generatedPassword = function(totalLength, number, lowerCase, upperCase) {
+const generatedPassword = function(totalLength, number, lowerCase, upperCase, special) {
   let passwordLength = Math.floor(Math.random() * (12 - 6) + 6);
   const numberList = '0123456789';
   const lowerCaseList = 'abcdefghijklmnopqrstuvwxyz';
   const upperCaseList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const specialChar = '!@#$%^&*+-'
   let letters = '';
 
   if (totalLength > 0) {
     passwordLength = totalLength;
   }
 
-  if (number) {
+  if (number ===  'true') {
     letters += numberList;
   }
 
-  if (lowerCase) {
+  if (lowerCase === 'true') {
     letters += lowerCaseList;
   }
 
-  if (upperCase) {
+  if (upperCase === 'true') {
     letters += upperCaseList;
   }
 
-  console.log('total length', passwordLength);
-  console.log('what is in string', letters);
+  if (special === 'true') {
+    letters += specialChar;
+  }
 
+  if (letters.length === 0) {
+    letters += lowerCaseList;
+  }
+
+  console.log(letters);
   let result = '';
   for (let i = 0; i < passwordLength; i++) {
     result += letters[(Math.floor(Math.random() * letters.length))];
   }
-
-  console.log(result);
+  letters = '';
   return result;
 };
-
-generatedPassword(null, false, true, true);
-
-module.exports = { generatedPassword };
