2d13d36d9fa938b15f5642f10e4a3ca6c360e006
Keith Ong <keith.ong5000@gmail.com>
2020-09-22 23:46:10 +0000

    Uncomment get /new route

--- server.js
+++ server.js
@@ -60,9 +60,9 @@ app.get("/", (req, res) => {
   res.render("websites");
 });
 
-// app.get("/new", (req, res) => {
-//   res.render("index");
-// });
+app.get("/new", (req, res) => {
+  res.render("index");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
