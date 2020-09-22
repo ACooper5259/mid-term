9ab95b184a86f3dd01f80c1982004f2deafc8b8e
Audrey Cooper <audreycooper@Audreys-MacBook-Pro.local>
2020-09-21 22:05:20 -0700

    add npm clipboard

--- public/scripts/copyToClipboard.js
+++ public/scripts/copyToClipboard.js
@@ -1,1 +1,13 @@
 
+$(document).ready(function() {
+  var copied = document.getElementById('copied');
+var clipboard = new ClipboardJS(copied);
+
+clipboard.on('success', function(e) {
+    console.log(e);
+});
+
+clipboard.on('error', function(e) {
+    console.log(e);
+});
+});
