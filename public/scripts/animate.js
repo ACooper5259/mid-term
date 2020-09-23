41587de824a9168c8592c313823bcb912a862a89
Audrey Cooper <audreycooper@Audreys-MacBook-Pro.local>
2020-09-23 11:48:44 -0700

    new animate: both forms display none by default

--- public/scripts/animate.js
+++ public/scripts/animate.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
   function compose() {
-    const generatePassword = document.getElementById("generate-password");
-    if (generatePassword.style.display === "none") {
-      generatePassword.style.display = "block";
-    } else {
+    const generatePassword = document.getElementById("new-site-forms");
+    if (generatePassword.style.display === "block") {
       generatePassword.style.display = "none";
+    } else {
+      generatePassword.style.display = "block";
     }
   }
+
+
 
   const passwordButton = document.getElementsByClassName("password-form");
   $(passwordButton).click(compose);
